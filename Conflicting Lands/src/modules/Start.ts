b160993b77719ec73c91be1c2e90b680858d0f63
MashaBut <42973147+MashaBut@users.noreply.github.com>
2019-07-28 21:13:15 +0300

    add part of algorithm for blocks

--- Conflicting Lands/src/modules/Start.ts
+++ Conflicting Lands/src/modules/Start.ts
@@ -1,11 +1,11 @@
-import {Game} from "./controllers/controller-main-module"
-import {take} from "rxjs/operators";
-import {fromEvent} from "rxjs";
-import {ManipulationWithDOM} from "./controllers/manipulations-with-dom";
-import {ConcealCanvas} from "./start/ux/scripts/hide-function";
-import {DiceRollerButton} from "./game/dice/dice-roller";
-import {diceCollection} from "./game/dice/dice";
-import {CreateImage} from "./start/ux/scripts/push-image";
+import { Game } from "./controllers/controller-main-module"
+import { take } from "rxjs/operators";
+import { fromEvent } from "rxjs";
+import { ManipulationWithDOM } from "./controllers/manipulations-with-dom";
+import { ConcealCanvas } from "./start/ux/scripts/hide-function";
+import { DiceRollerButton } from "./game/dice/dice-roller";
+import { diceCollection } from "./game/dice/dice";
+import { CreateImage } from "./start/ux/scripts/push-image";
 
 import "./start/ux/css/main.css";
 import "./start/ux/css/players.css";
@@ -15,30 +15,30 @@ import "../assets/images/VS.jpg";
 import "../assets/images/dicesAnimation.gif";
 import { url } from "inspector";
 
-let playGame = require ('../assets/sounds/playGame.wav');
-let soundForDice = require ('../assets/sounds/rollTheDice2.wav');
+let playGame = require('../assets/sounds/playGame.wav');
+let soundForDice = require('../assets/sounds/rollTheDice2.wav');
 
 ConcealCanvas.hideGamePage();
 let game: Game = new Game();
 let diceRollerButton = new DiceRollerButton();
 
 fromEvent(ManipulationWithDOM.writeNames, 'click')
-    .pipe(take(1))
-    .subscribe(() => {
-        game.setPlayerNames();
-        
-        ConcealCanvas.hideStartPage();
-        ManipulationWithDOM.playSound(playGame);
-        CreateImage.createImage();
-    })
+  .pipe(take(1))
+  .subscribe(() => {
+    game.setPlayerNames();
+
+    ConcealCanvas.hideStartPage();
+    //  ManipulationWithDOM.playSound(playGame);
+    CreateImage.createImage();
+  })
 
 fromEvent(ManipulationWithDOM.tossDice, 'click')
-    .subscribe(() => {
-        CreateImage.returmAnimate();
-      //  ManipulationWithDOM.playSound(soundForDice);
-        ManipulationWithDOM.disabledButtonDice();
-        diceRollerButton.roll(diceCollection);
-        game.createPositionsBlockForMap(diceRollerButton.numberOfDices());
-        game.draw();
-        game.turnTime();
-    })   
+  .subscribe(() => {
+    CreateImage.returmAnimate();
+    //  ManipulationWithDOM.playSound(soundForDice);
+    ManipulationWithDOM.disabledButtonDice();
+    diceRollerButton.roll(diceCollection);
+    game.createPositionsBlockForMap(diceRollerButton.numberOfDices());
+    game.draw();
+    game.turnTime();
+  })   
--- Conflicting Lands/src/modules/controllers/controller-main-module.ts
+++ Conflicting Lands/src/modules/controllers/controller-main-module.ts
@@ -1,21 +1,21 @@
-import {Player} from "../player";
-import {Identification} from "../start/write-names";
-import {ManipulationWithDOM} from "./manipulations-with-dom";
-import {fromEvent, Observable} from "rxjs";
-import {Directions} from './key-designations';
-import {CanvasDraw} from "../game/work-with-canvas/draw";
-import {CoordinateTransformation} from "../game/work-with-canvas/create";
-import {Position} from "../game/work-with-canvas/create-position";
+import { Player } from "../player";
+import { Identification } from "../start/write-names";
+import { ManipulationWithDOM } from "./manipulations-with-dom";
+import { fromEvent, Observable } from "rxjs";
+import { Directions } from './key-designations';
+import { CanvasDraw } from "../game/work-with-canvas/draw";
+import { CoordinateTransformation } from "../game/work-with-canvas/create";
+import { Position } from "../game/work-with-canvas/create-position";
 import { threadId } from "worker_threads";
 
-let enterSound = require ('../../assets/sounds/soundForBlock3.wav');
-let movementsOfBlock = require ('../../assets/sounds/moveblock2.wav');
+let enterSound = require('../../assets/sounds/soundForBlock3.wav');
+let movementsOfBlock = require('../../assets/sounds/moveblock2.wav');
 export class Game {
-    
+
     private player1: Player;
     private player2: Player;
     private currentPlayer: Player;
-    private keyDown: Observable <Event> = fromEvent(document,'keydown');
+    private keyDown: Observable<Event> = fromEvent(document, 'keydown');
     private flagGame: boolean = true;
     private timer: any;
 
@@ -27,7 +27,7 @@ export class Game {
     private currentPositionforBlockOnMap: any;
     private x: number;
     private y: number;
-    private a: string = "Green";
+    private currentColorBlockOnMap: string = "Orange";
     constructor() {
         this.initCanvas();
         this.manipulationKeyBoard();
@@ -38,36 +38,35 @@ export class Game {
     }
 
     public createPositionsBlockForMap(dice: number[]) {
-        this.size = CoordinateTransformation.conversionToPixels(this.canvasDraw.aspectRatio-2,dice);
+        this.size = CoordinateTransformation.conversionToPixels(this.canvasDraw.aspectRatio - 2, dice);
         this.calculatePosition();
     }
 
     private calculatePosition() {
         let coord = this.firstStep(this.size);
-        this.currentPositionforBlockOnMap=[coord[0],coord[1],this.size[0],this.size[1]];
+        this.currentPositionforBlockOnMap = [coord[0], coord[1], this.size[0], this.size[1]];
     }
     private firstStep(coordinates: number[]): number[] {
-        this.x = this.currentPlayer.getXCoordinate()-this.size[0]-1;
-        this.y = this.currentPlayer.getYCoordinate()-this.size[1]-1;
+        this.x = this.currentPlayer.getXCoordinate() - this.size[0] - 1;
+        this.y = this.currentPlayer.getYCoordinate() - this.size[1] - 1;
         this.x > 0 ? this.x : this.x = 1;
         this.y > 0 ? this.y : this.y = 1;
-        return [this.x,this.y];
+        return [this.x, this.y];
     }
 
-    private endOfturn(){
+    private endOfturn() {
         ManipulationWithDOM.undisabledButtonDice();
-        this.canvasDraw.redraw(this.currentPositionforBlockOnMap,this.currentPlayer.getColor());
+        this.canvasDraw.redraw(this.currentPositionforBlockOnMap, this.currentPlayer.getColor());
         this.position.saveBlockOnMap(this.currentPositionforBlockOnMap, this.currentPlayer.getColor());
-        console.log(this.currentPositionforBlockOnMap);
         this.canvasDraw.saveCanvasToImage();
-        if(this.currentPlayer.isFirstMove()) {
+        if (this.currentPlayer.isFirstMove()) {
             this.currentPlayer.firstMove = false;
         }
         this.changePlayer();
     }
 
-    private changePlayer():void {
-        if(this.flagGame) {
+    private changePlayer(): void {
+        if (this.flagGame) {
             this.flagGame = false;
             this.currentPlayer = this.player2;
         }
@@ -78,7 +77,7 @@ export class Game {
     }
 
     public turnTime() {
-        this.timer = setTimeout(() => this.endOfturn(),20000);
+        this.timer = setTimeout(() => this.endOfturn(), 20000);
     }
 
     public setPlayerNames(): void {
@@ -87,12 +86,12 @@ export class Game {
 
         this.player1 = new Player(Identification.setName(namePlayer1, "Player 1"), "Red", 1, ManipulationWithDOM.canvas.height);
         this.player2 = new Player(Identification.setName(namePlayer2, "Player 2"), "Blue", ManipulationWithDOM.canvas.width, 1);
-        ManipulationWithDOM.nameplayer1.innerHTML=this.player1.getName();
-        ManipulationWithDOM.nameplayer2.innerHTML=this.player2.getName();
-        this.currentPlayer=this.player1;
+        ManipulationWithDOM.nameplayer1.innerHTML = this.player1.getName();
+        ManipulationWithDOM.nameplayer2.innerHTML = this.player2.getName();
+        this.currentPlayer = this.player1;
     }
 
-    public manipulationKeyBoard():void {
+    public manipulationKeyBoard(): void {
         this.keyDown
             .subscribe((e: KeyboardEvent) => {
                 ManipulationWithDOM.disableStandardKeyOperation(e);
@@ -101,70 +100,70 @@ export class Game {
     }
 
     private setBlockPositionOnMap(keyCode: KeyboardEvent): void {
-        switch(keyCode.keyCode) {
+        switch (keyCode.keyCode) {
             case 16: {
                 this.size = CoordinateTransformation.turnSize();
-                if(this.currentPlayer.isFirstMove()) {
+                if (this.currentPlayer.isFirstMove()) {
                     this.calculatePosition();
                 }
                 else {
                     this.currentPositionforBlockOnMap[2] = this.size[0];
                     this.currentPositionforBlockOnMap[3] = this.size[1];
                 }
-                this.a = this.position.createPositionForCurrentPlayer(this.currentPositionforBlockOnMap,this.currentPlayer.getColor(),"Green");
+                this.currentColorBlockOnMap = this.position.createPositionForCurrentPlayer(this.currentPositionforBlockOnMap, this.currentPlayer.getColor(), "Green");
                 this.draw();
-          //      ManipulationWithDOM.playSound(movementsOfBlock);
+                //      ManipulationWithDOM.playSound(movementsOfBlock);
             }
                 break;
             case Directions.Down: {
-                    if(!this.currentPlayer.isFirstMove()) {
-                    this.currentPositionforBlockOnMap[1] +=this.canvasDraw.aspectRatio;
-                    this.a = this.position.createPositionForCurrentPlayer(this.currentPositionforBlockOnMap,this.currentPlayer.getColor(),"Green");
+                if (!this.currentPlayer.isFirstMove()) {
+                    this.currentPositionforBlockOnMap[1] += this.canvasDraw.aspectRatio;
+                    this.currentColorBlockOnMap = this.position.createPositionForCurrentPlayer(this.currentPositionforBlockOnMap, this.currentPlayer.getColor(), "Green");
                     this.draw();
-               //     ManipulationWithDOM.playSound(movementsOfBlock);
-                    }
+                    //     ManipulationWithDOM.playSound(movementsOfBlock);
                 }
-                    break;
-            case Directions.Up: {
-                if(!this.currentPlayer.isFirstMove()) {
-                    this.currentPositionforBlockOnMap[1] -= this.canvasDraw.aspectRatio;
-                    this.a = this.position.createPositionForCurrentPlayer(this.currentPositionforBlockOnMap,this.currentPlayer.getColor(),"Green");
-                    this.draw();
-                }
-              //  ManipulationWithDOM.playSound(movementsOfBlock);
             }
                 break;
-            
-            case Directions.Right: {
-                if(!this.currentPlayer.isFirstMove()) {
-                this.currentPositionforBlockOnMap[0] +=this.canvasDraw.aspectRatio;
-                this.a = this.position.createPositionForCurrentPlayer(this.currentPositionforBlockOnMap,this.currentPlayer.getColor(),"Green");
-                this.draw();
+            case Directions.Up: {
+                if (!this.currentPlayer.isFirstMove()) {
+                    this.currentPositionforBlockOnMap[1] -= this.canvasDraw.aspectRatio;
+                    this.currentColorBlockOnMap = this.position.createPositionForCurrentPlayer(this.currentPositionforBlockOnMap, this.currentPlayer.getColor(), "Green");
+                    this.draw();
                 }
-              //  ManipulationWithDOM.playSound(movementsOfBlock);
+                //  ManipulationWithDOM.playSound(movementsOfBlock);
+            }
+                break;
+
+            case Directions.Right: {
+                if (!this.currentPlayer.isFirstMove()) {
+                    this.currentPositionforBlockOnMap[0] += this.canvasDraw.aspectRatio;
+                    this.currentColorBlockOnMap = this.position.createPositionForCurrentPlayer(this.currentPositionforBlockOnMap, this.currentPlayer.getColor(), "Green");
+                    this.draw();
+                }
+                //  ManipulationWithDOM.playSound(movementsOfBlock);
             }
                 break;
             case Directions.Left: {
-                if(!this.currentPlayer.isFirstMove()) {
-                this.currentPositionforBlockOnMap[0] -=this.canvasDraw.aspectRatio;
-                this.a = this.position.createPositionForCurrentPlayer(this.currentPositionforBlockOnMap,this.currentPlayer.getColor(),"Green");
-                this.draw();
+                if (!this.currentPlayer.isFirstMove()) {
+                    this.currentPositionforBlockOnMap[0] -= this.canvasDraw.aspectRatio;
+                    this.currentColorBlockOnMap = this.position.createPositionForCurrentPlayer(this.currentPositionforBlockOnMap, this.currentPlayer.getColor(), "Green");
+                    this.draw();
                 }
-              //  ManipulationWithDOM.playSound(movementsOfBlock);
+                //  ManipulationWithDOM.playSound(movementsOfBlock);
             }
                 break;
             case Directions.Enter: {
-                ManipulationWithDOM.playSound(enterSound);
+                // ManipulationWithDOM.playSound(enterSound);
                 clearTimeout(this.timer);
                 this.endOfturn();
             }
                 break;
-            default :
+            default:
                 break;
         }
     }
 
     public draw(): void {
-       this.canvasDraw.redraw(this.currentPositionforBlockOnMap,this.a);  
+        this.canvasDraw.redraw(this.currentPositionforBlockOnMap, this.currentColorBlockOnMap);
     }
 }
